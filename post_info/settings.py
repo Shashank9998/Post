047ec15c71b0f50a95b4cdf39e072fdb927f6b91
Shashank9998 <shashankpatel9998@gmail.com>
2026-06-02 19:05:20 +0530

    update on 02/06/2026

--- post_info/settings.py
+++ post_info/settings.py
@@ -125,3 +125,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
